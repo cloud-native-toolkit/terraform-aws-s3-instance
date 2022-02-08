a4f68aa37f2a0b6727b28d74b5f640952f69eca1
Sasikanth G <bgumpana@in.ibm.com>
2022-02-08 20:25:16 +0530

    AWS S3 instance
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -5,4 +5,4 @@ REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/
 aws configure set region ${REGION}
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-exit 1
+exit 0
