8a49d12f9dc9a8f9f683b6ba903874893186751a
Sasikanth G <bgumpana@in.ibm.com>
2022-02-09 21:49:03 +0530

    AWS S3 instance
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,12 +5,12 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        s3_bucket_id = module.aws-s3-instance.s3_bucket_id
-        s3_bucket_arn = module.aws-s3-instance.s3_bucket_arn
-        s3_bucket_bucket_domain_name = module.aws-s3-instance.s3_bucket_bucket_domain_name
-        s3_bucket_bucket_regional_domain_name = module.aws-s3-instance.s3_bucket_bucket_regional_domain_name
-        s3_bucket_hosted_zone_id = module.aws-s3-instance.s3_bucket_hosted_zone_id
-        s3_bucket_region = module.aws-s3-instance.s3_bucket_region
+        s3_bucket_id = module.aws_s3_instance.s3_bucket_id
+        s3_bucket_arn = module.aws_s3_instance.s3_bucket_arn
+        s3_bucket_bucket_domain_name = module.aws_s3_instance.s3_bucket_bucket_domain_name
+        s3_bucket_bucket_regional_domain_name = module.aws_s3_instance.s3_bucket_bucket_regional_domain_name
+        s3_bucket_hosted_zone_id = module.aws_s3_instance.s3_bucket_hosted_zone_id
+        s3_bucket_region = module.aws_s3_instance.s3_bucket_region
       })
     }
   }
